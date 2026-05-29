94c720252f4bab29e0c7ccee35874f2b3bef58a8
Belenuah <belencastrocalzado@gmail.com>
2026-05-29 01:10:48 -0400

    Arreglo de trabajo 1 y trabajo 2
    Cambios

--- Proc/Script_Procesamiento.R
+++ Proc/Script_Procesamiento.R
@@ -20,95 +20,140 @@ elsoc2022 <- base_elsoc %>%
 view(elsoc2022)
 
 #3. Seleccionamos variables.-
-elsoc2022_variables <- elsoc2022 %>%
+elsoc6variables <- elsoc2022 %>%
   select(
     sexo = sexo_enc,
-    edad   = edad_enc,     
-    conf_medios       = c05_12,     
+    edad   = edad_enc,   
     info_politica_medios = c14_02,
-    niveleduc = m01)
+    niveleduc = m01,
+    sumision_auto = c18_05,
+    agresion_auto = c37_05,
+    convencionalismo = r12_06)
 
 #4. Eliminamos los NAs o casos perdidos.-
 
-elsoc2022variables_limpia <- elsoc2022_variables %>%
+elsoc6limpia <- elsoc6variables %>%
   mutate(
-    edad_= na_if(edad, -888),
+    edad = na_if(edad, -888),
     edad = na_if(edad, -999),
-    conf_medios = na_if(conf_medios, -888),
-    conf_medios = na_if(conf_medios, -999),
     info_politica_medios = na_if(info_politica_medios, -888),
     info_politica_medios = na_if(info_politica_medios, -999),
+    sumision_auto = na_if(sumision_auto, -888),
+    sumision_auto = na_if(sumision_auto, -999),
     niveleduc = na_if(niveleduc, -888),
-    niveleduc = na_if(niveleduc, -999)) %>% 
-  drop_na(edad, conf_medios, info_politica_medios, niveleduc)   
+    niveleduc = na_if(niveleduc, -999),
+    agresion_auto = na_if(agresion_auto, -888),
+    agresion_auto = na_if(agresion_auto, -999),
+    convencionalismo = na_if(convencionalismo, -888),
+    convencionalismo = na_if(convencionalismo, -999))%>% 
+  drop_na(edad,sexo,info_politica_medios,niveleduc,sumision_auto,agresion_auto,convencionalismo)
+
+view(elsoc6limpia)
 
 #5. Recodificamos la variable "EDAD" en grupos etarios.-
 
-elsoc2022_final <- elsoc2022variables_limpia %>%
-  mutate(rango_etario = case_when(
-    edad >= 18 & edad <= 25 ~ "Jóvenes",
-    edad >= 26 & edad <= 59 ~ "Adultos",
-    edad >= 60 ~ "Adultos Mayores",
-    TRUE ~ NA_character_)) %>%
-  mutate(rango_etario = factor(rango_etario, 
-                               levels = c("Jóvenes", "Adultos", "Adultos Mayores")))
-
-table(elsoc2022_final$rango_etario)
-by(elsoc2022_final$edad, elsoc2022_final$rango_etario, summary)
-
-#6. Recodificamos la variable "NIVEL EDUCACIONAL" en grupos de nivel educacional.-
-
-elsoc22_final <- elsoc2022_final %>%
+elsoc6limpia <- elsoc6limpia %>%
   mutate(
-    nivel_educ = case_when(
-      niveleduc <= 3 ~ "Básica",           
-      niveleduc >= 4 & niveleduc <= 5 ~ "Media",      
-      niveleduc >= 6 & niveleduc <= 10 ~ "Superior",  
+    niveleduc = suppressWarnings(
+      as.numeric(as.character(niveleduc))))
+
+elsoc6 <- elsoc6limpia %>%
+  mutate(
+    
+    tramo_edad = case_when(
+      edad >= 18 & edad <= 29 ~ "Jóvenes",
+      edad >= 30 & edad <= 59 ~ "Adultos",
+      edad >= 60 & edad <= 78 ~ "Adultos Mayores",
       TRUE ~ NA_character_),
-    nivel_educ = factor(nivel_educ, levels = c("Básica", "Media", "Superior")))
+    
+    tramo_edad = factor(
+      tramo_edad,
+      levels = c(
+        "Jóvenes",
+        "Adultos",
+        "Adultos Mayores")),
+    
+    niveleducacion = case_when(
+      niveleduc >= 1 & niveleduc <= 3 ~ "Básica",
+      niveleduc >= 4 & niveleduc <= 5 ~ "Media",
+      niveleduc >= 6 & niveleduc <= 10 ~ "Superior",
+      TRUE ~ NA_character_))
 
-#7.Descriptivos.-
-#7.1 Promedio de confianza según Sexo y Rango Etario.
-elsoc22_final %>%
-  group_by(sexo, rango_etario) %>%
-  summarise(promedio_confianza = mean(conf_medios, na.rm = TRUE),
-            desviacion = sd(conf_medios, na.rm = TRUE),
-            n = n()) %>%
-  ungroup()
+#7.Tablas.-
 
-#7.2 Promedio de confianza según Nivel Educacional.
-elsoc22_final %>%
-  group_by(nivel_educ) %>%
-  summarise(promedio_confianza = mean(conf_medios, na.rm = TRUE))
+tabla_descriptivos  <-  elsoc6 %>%
+  select (sueldo, edad) %>%
+  pivot_longer (cols = everything(), names_to = “variable”, values_to = “valor”) %>%
+  group_by(variable) %>%
+  summarise(
+    minimo = min(valor),
+    media = mean(valor),
+    mediana = median(valor),
+    maximo = max(valor),
+    SD = sd(valor),
+  )
 
-#8. Gráficos.
+
+#7.1 Tabla de medidas de Tendencia Central
+p_load(psych)
+
+tabla_mtc <- describe(elsoc6[, c("sumision_auto", "info_politica_medios", "agresion_auto", "convencionalismo")])
+
+tabla_mtc <- tabla_mtc %>% 
+  select(n, mean, sd, median, min, max)
+
+print(tabla_mtc)
+
+#7.2 Tabla de frecuencias.
+# Frecuencia para Sexo
+tabla_sexo <- elsoc6 %>%
+  count(sexo) %>%
+  mutate(porcentaje = n / sum(n) * 100)
+
+# Frecuencia para Tramo Edad
+tabla_tramoedad <- elsoc6 %>%
+  count(tramo_edad) %>%
+  mutate(porcentaje = n / sum(n) * 100)
+
+# Frecuencia para Nivel Educacional
+tabla_educ <- elsoc6 %>%
+  count(niveleducacion) %>%
+  mutate(porcentaje = n / sum(n) * 100)
+
+# Ver las tablas
+print(tabla_sexo)
+print(tabla_tramoedad)
+print(tabla_educ)
+
+#8. Gráficos.-
 
 #8.1 Gráfico para Sexo
-ggplot(elsoc22_final, aes(x = as.factor(sexo), y = conf_medios, fill = as.factor(sexo))) +
+ggplot(elsoc6, aes(x = as.factor(sexo), y = info_politica_medios, fill = as.factor(sexo))) +
   stat_summary(fun = "mean", geom = "bar") +
   geom_text(stat = "summary", fun = "mean", aes(label = round(..y.., 2)), vjust = -0.5) +
-  labs(title = "Promedio de Confianza en Medios de Comunicacion tradicionales según Sexo",
+  labs(title = "Promedio de Uso de Medios de Comunicacion para informarse sobre política según Sexo",
        x = "Sexo (1=Hombre, 2=Mujer)", 
-       y = "Promedio de Confianza",
+       y = "Promedio de uso",
        fill = "Sexo") +
   theme_classic()
 
 #8.2 Gráfico para Rango Etario
-ggplot(elsoc22_final, aes(x = rango_etario, y = conf_medios, fill = rango_etario)) +
+ggplot(elsoc6, aes(x = tramo_edad, y = info_politica_medios, fill = tramo_edad)) +
   stat_summary(fun = "mean", geom = "bar") +
   geom_text(stat = "summary", fun = "mean", aes(label = round(..y.., 2)), vjust = -0.5) +
-  labs(title = "Promedio de Confianza en Medios de Comunicacion tradicionales por Rango Etario",
-       x = "Grupos de Rango Etario",
-       y = "Promedio de Confianza") +
+  labs(title = "Promedio de Uso de Medios de Comunicacion para informarse sobre política por Tramo de Edad",
+       x = "Tramos Edad",
+       y = "Promedio de uso") +
   theme_minimal() +
-  scale_fill_brewer(palette = "Set2")
+  scale_fill_brewer(palette = "Set3")
 
 #8.3 Gráfico para Nivel Educacional
-ggplot(elsoc22_final, aes(x = nivel_educ, y = conf_medios, fill = nivel_educ)) +
+ggplot(elsoc6, aes(x = niveleducacion, y = info_politica_medios, fill = niveleducacion)) +
   stat_summary(fun = "mean", geom = "bar") +
   geom_text(stat = "summary", fun = "mean", aes(label = round(..y.., 2)), vjust = -0.5) +
-  labs(title = "Promedio de Confianza en Medios de Comunicacion tradicionales por Nivel Educacional",
+  labs(title = "Promedio de uso de Medios de Comunicacion para informarse sobre politica por Nivel Educacional",
        x = "Nivel de Educación",
-       y = "Promedio de Confianza") +
+       y = "Promedio de uso") +
   theme_light() +
   scale_fill_viridis_d(option = "plasma")
+
